0fe9bd8bb9898c950820d336c1b9adeffa8a4395
Joel Wise <joelwise.ndungu@student.moringaschool.com>
2026-04-16 21:49:58 +0300

    Initial working version of the website without synonyms and pronunciation

--- index.js
+++ index.js
@@ -1,18 +1,55 @@
-const dictionaryAPI = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
-const buttton = document.getElementById("look-for-word-in-dictionary")
+const button = document.getElementById("look-for-word-in-dictionary")
 const userInput = document.getElementById("input-word")
 
 function fetchWordData (word){
+const dictionaryAPI = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
     fetch(dictionaryAPI)
-    .then(response =>response.json())
-    .then(data ()=>{
-
+    .then(response =>{
+        if (!response.ok){
+            throw new Error("Word not found")
+        }
+        
+        return response.json()
     })
+    .then(data =>{
+        userInput.value = ""
+        displayWordData(data)
+    })
+    .then( () => {
+        console.log("Word found successfully!")
+    })
+    
     .catch(error =>{
         console.log("Error: Cannot find the word you were looking for. Try searching for your word on google")
     })
 }
 
-buttton.addEventListener("click" ,() =>{
+function displayWordData (data){
+  const wordMeaning = document.getElementById("word-meaning")
+  wordMeaning.innerHTML = ""
+  
+  const word = data[0].word
+  const definition = data[0].meanings[0].definitions[0].definition
+  const synonyms = data[0].meanings[0].synonyms ||[]
+  const pronunciation = data[0].phonetic
+
+  const wordTitle = document.createElement("h2")
+  const meaning = document.createElement("p")
+  const similar = document.createElement("p")
+  
+  wordTitle.textContent = word
+  meaning.textContent = `definition: ${definition}`
+  similar.textContent = synonyms
+
+  wordMeaning.append(meaning)
+  wordMeaning.append(wordTitle)
+  wordMeaning.append(synonyms)
+  wordMeaning.append(pronunciation)
+}
+
+button.addEventListener("click" ,(event) =>{
+        event.preventDefault()
+    
+    const word = userInput.value
     fetchWordData(word)
 })
